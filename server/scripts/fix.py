070ab3f7772d6ea7c6088442abcff08321d3e963
Stanley Bankesie <stanleyetornam@gmail.com>
2026-07-30 07:44:21 +0000

    ttt

--- server/scripts/fix.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open('patch.js', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('\\`', '`')
-
-with open('patch.js', 'w', encoding='utf-8') as f:
-    f.write(text)
